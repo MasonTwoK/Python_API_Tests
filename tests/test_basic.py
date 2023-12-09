4d4fee5d728fd16ff9346da879bc5c97fd8a199e
MasonTwoK <willhamilton1991@gmail.com>
2023-12-09 22:16:23 +0200

    !U Added tests for length and coverage for GET random joke

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,15 @@
-# https://postman-rest-api-learner.glitch.me//info
+# Home Task:
 # 1. Покрити api chuck norris https://api.chucknorris.io/
 # 2. тести створити атомарі
 # 3. зробити фікстуру
 # 4. зробити маркери (лейбли)
 # 5. прописати їх в pytest.ini
 # 6. проранити через terminal pytest використовуючи оператори AND, OR, NOT
+
+# Questions:
+# 1. Should I check data type for response fields?
+# 2. Is it OK that for getting joke I get key categories NOT category, and data type is list
+
 import pytest
 import requests
 
@@ -30,6 +35,7 @@ def test_norris_get_categories_response_body_check():
     resp = requests.get(
         url='https://api.chucknorris.io/jokes/categories'
     )
+    assert len(resp.json()) == 16
     assert resp.json() == ['animal', 'career', 'celebrity', 'dev', 'explicit', 'fashion', 'food', 'history', 'money',
                            'movie', 'music', 'political', 'religion', 'science', 'sport', 'travel']
 
@@ -42,15 +48,25 @@ def test_norris_get_by_category():
     for category in categories:
         resp = requests.get(
             url=f'https://api.chucknorris.io/jokes/random?category={category}')
+
         assert resp.status_code == 200
-        print(resp.json())
 
 
 # Jokes Random
 def test_norris_get_random_success_code():
     resp = requests.get(
         url='https://api.chucknorris.io/jokes/random')
-    assert resp.status_code == 200
+
+    assert resp.status_code == 200, 'Returned status code is not 200'
+    assert len(resp.json()) == 7, 'Length of response body size is not 7'
+    # assert resp.json()['categories'] != [], 'Category is not set' #  "BUG #1: Empty Category is present"
+    # assert len(resp.json()['categories']) == 1, 'Length of categories is not equal 1'
+    assert resp.json()['created_at'] is not None
+    assert resp.json()['icon_url'] is not None
+    assert resp.json()['id'] is not None
+    assert resp.json()['updated_at'] is not None
+    assert resp.json()['url'] is not None
+    assert resp.json()['value'] is not None
 
 
 def test_norris_get_by_query_success_code():
@@ -58,9 +74,11 @@ def test_norris_get_by_query_success_code():
     resp = requests.get(
         url=f'https://api.chucknorris.io/jokes/search?query={test_query}'
     )
+
     assert resp.status_code == 200
+    assert resp.json() is not None
     assert resp.json()['total'] != 0
-    assert resp.json()['result'] != []
+    assert resp.json()['result'] is not None
     assert resp.json()['result'][0]['created_at'] is not None
     assert resp.json()['result'][0]['icon_url'] is not None
     assert resp.json()['result'][0]['id'] is not None
@@ -69,10 +87,14 @@ def test_norris_get_by_query_success_code():
     assert resp.json()['result'][0]['value'] is not None
 
 
-@pytest.mark.skip("BUG: Empty Category is present")
+@pytest.mark.skip("BUG #1: Empty Category is present")
 def test_norris_get_by_query_category_field_check():
     test_query = 'Chuck'
     resp = requests.get(
         url=f'https://api.chucknorris.io/jokes/search?query={test_query}'
     )
+
     assert resp.json()['result'][0]['categories'] != [], 'Category is be empty'
+
+
+
