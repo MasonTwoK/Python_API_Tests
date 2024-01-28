5d1dd6c4a87615dfbea4c6026029ca500652af7a
MasonTwoK <willhamilton1991@gmail.com>
2024-01-28 22:36:10 +0200

    !U petstore api refactor: tests separated to different classes

--- api_petstore/tests/test_store.py
+++ api_petstore/tests/test_store.py
@@ -2,21 +2,27 @@
 import pytest
 
 
-# Questions:
-# Q1: Do I need create a separate project for separate API coverage?
-
 @pytest.mark.petstore_store
-class TestStore:
-    def test_post_store_order_success(self, petstore_post_store_order):
+class TestStorePost:
+    @staticmethod
+    def test_post_store_order_success(petstore_post_store_order):
         assert petstore_post_store_order.status_code == 200
 
+    @staticmethod
     @pytest.mark.skip(reason='BUG #1 Unexpected error code appear while sending request with out json body')
     @pytest.mark.negative_scenario
-    def test_post_store_order_without_body(self, petstore_post_store_order_without_body):
+    def test_post_store_order_without_body(petstore_post_store_order_without_body):
         assert petstore_post_store_order_without_body.status_code == 400
 
-    def test_get_store_order_success(self, petstore_get_store_order):
+
+class TestStoreGet:
+    @staticmethod
+    def test_get_store_order_success(petstore_get_store_order):
         assert petstore_get_store_order.status_code == 200
 
-    def test_delete_store_order_success(self, petstore_delete_store_order):
+
+class TestStoreDelete:
+
+    @staticmethod
+    def test_delete_store_order_success(petstore_delete_store_order):
         assert petstore_delete_store_order.status_code == 200
